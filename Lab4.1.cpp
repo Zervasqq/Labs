19ee764879ae3bfc59c771f11c491851b02b8fa1
Zervasqq <themach.2006@gmail.com>
2024-09-25 18:27:11 +0300

    Lab4 (1,2,3). 5 вариант

---
+++ Lab4.1.cpp
@@ -0,0 +1,28 @@
+﻿// 5 вариант
+// Задача 1. Ввести натуральные числа A, B и C. Если A меньше B и B меньше C, то вывести C-B-A, в противном случае если A кратно C, то вывести A/С+B, в остальных случаях вывести A+B+C.
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	cin >> a >> b >> c;
+	if (a < b && b < c)
+	{
+		cout << c - b - a << endl;
+	}
+	else if (a % c == 0)
+	{
+		cout << a / c + b << endl;
+	}
+	else
+	{
+		cout << a + b + c << endl;
+	}
+}
+
+
+
---
+++ Lab4.2.cpp
@@ -0,0 +1,38 @@
+﻿// 5 вариант
+// Задача. Ввести число N, обозначающее код товара в магазине. При помощи оператора switch вывести название товара. Код товара и название придумать самостоятельно. Минимум 5 штук. Предусмотреть обработку ошибочного ввода N.
+
+
+#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите код товара." << endl;
+    int n;
+    cin >> n;
+    switch (n)
+    {
+    case 1:
+        cout << "Гречка" << endl;
+        break;
+    case 2:
+        cout << "Чипсы Lays" << endl;
+        break;
+    case 3:
+        cout << "Рис" << endl;
+        break;
+    case 4:
+        cout << "Макароны" << endl;
+        break;
+    case 5:
+        cout << "Хлеб" << endl;
+        break;
+    default:
+        cout << "Неверный код товара." << endl;
+        break;
+    }
+}
+
