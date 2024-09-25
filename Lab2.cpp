908b2efc6f0b82c756d27a7eda13e90e7e860bdd
Zervasqq <themach.2006@gmail.com>
2024-09-25 16:23:44 +0300

    Lab2
    Объем прямоугольного параллелепипеда

---
+++ Lab2.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int a, b, c;
+	cout << "Для вычисления объёма введите стороны прямоугольного параллелепипеда." << endl;
+	cin >> a >> b >> c;
+	if (a > 0 && a < 10*10*10*10 && b > 0 && b < 10 * 10 * 10 * 10 && c > 0 && c < 10 * 10 * 10 * 10)
+	{
+		cout << "Объём прямоугольного параллелепипеда равен " << a * b * c << "." << endl;
+	}
+	else
+	{
+		cout << "Ошибка: неверные значения сторон." << endl;
+	}
+   
+}
