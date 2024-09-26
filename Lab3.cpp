bf7dd648eb51b4bf0ff74402a600e870ff6ed5e1
Zervasqq <themach.2006@gmail.com>
2024-09-26 23:45:38 +0300

    Lab3 5 вариант

---
+++ Lab3.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int i, x;
+	cout << "Введите число." << endl;
+	cin >> x;
+	cout << "Введите бит числа, который Вы хотите заменить на 0." << endl;
+	cin >> i;
+	if (x > 0 and x < 10 * 10 * 10 * 10 * 10 * 10 * 10 * 10 * 10)
+	{
+		cout << "После замены число преобразовалось в " << (x & ~(1 << i)) << endl;
+	}
+	else
+	{
+		cout << "Некорретное значение переменной x.";
+	}
+}
